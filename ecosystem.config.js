9dcdfd2306b7f998a6d139a4bf2787f29719379c
Ryan Andonian <ryanandonian@gmail.com>
2025-10-04 18:57:56 -0700

    fix: more config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'ttrpg-preroller',
-      script: './dist/app.js',
+      script: './dist/src/app.js',
       instances: 1,
       exec_mode: 'fork',
       watch: true,
